9a82e94a2f9125c1bb06fbb329648c505df57432
Adam Brown <adam@novoda.com>
2016-10-25 09:06:13 +0100

    basic wake on lan webapp, server and client

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const httpServer = require('http').Server(app);
+const wol = require('wake_on_lan');
+const http = require('request');
+const basicAuth = require('basic-auth-connect');
+
+const secrets = require('./secrets');
+
+app.use("/public", express.static(__dirname + '/public'));
+
+app.use(basicAuth(secrets.auth.username, secrets.auth.password));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/wake', function(req, res){
+  wol.wake('94:DE:80:AA:CF:63');
+  res.end();
+});
+
+app.get('/shutdown', function(req, res){
+  http('http://192.168.0.8:3000', function(err, res) {
+    res.end();
+  });
+});
+
+httpServer.listen(3001, function(){
+  console.log('listening on *:3001');
+});
